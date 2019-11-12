1316d7e30abff0531b719d1a7b86991a20eb6150
Jakub Turek <jkbturek@gmail.com>
2019-11-12 11:17:46 +0100

    Implement fetching services in TODOService

--- TCRDemoTests/TCRDemoTests.swift
+++ TCRDemoTests/TCRDemoTests.swift
@@ -2,25 +2,48 @@ import XCTest
 import Foundation
 @testable import TCRDemo
 
-func fetchTODOs(_ completion: @escaping (String) -> Void) {
-    let url = "https://jsonplaceholder.typicode.com/todos"
+class RequestExecutor {
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
 
-    let task = URLSession.shared.dataTask(with: URL(string: url)!) { data, response, error in
-        if let data = data, let result = String(data: data, encoding: .utf8) {
-            completion(result)
+    func get(url: URL, _ completion: @escaping (Data?) -> Void) {
+        let task = session.dataTask(with: url) { data, _, _ in
+            completion(data)
+        }
+
+        task.resume()
+    }
+
+    private let session: URLSession
+}
+
+class TODOService {
+    init(requestExecutor: RequestExecutor = RequestExecutor()) {
+        self.requestExecutor = requestExecutor
+    }
+
+    func fetchTODOs(_ completion: @escaping (String) -> Void) {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/todos")!
+
+        requestExecutor.get(url: url) { data in
+            if let data = data, let result = String(data: data, encoding: .utf8) {
+                completion(result)
+            }
         }
     }
 
-    task.resume()
+    private let requestExecutor: RequestExecutor
 }
 
-class TODOFetcherTests: XCTestCase {
+class TODOServiceTests: XCTestCase {
 
     func testFetchesTodos() {
         var todos = ""
         let todosFetched = expectation(description: "todos fetched")
+        let sut = TODOService()
 
-        fetchTODOs {
+        sut.fetchTODOs {
             todos = $0
             todosFetched.fulfill()
         }
